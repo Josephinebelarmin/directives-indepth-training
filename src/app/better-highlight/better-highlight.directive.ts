17bfeb8cc33dde82eef0cb8dbce768a68159c1bf
Josephinebelarmin <josephine.belarmin@gmail.com>
2022-03-31 07:04:01 -0400

    hostListener exapmle mouseenter and mouseLeave

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,8 +7,16 @@ export class BetterHighlightDirective implements OnInit {
 
   constructor(private elRefer: ElementRef, private renderer: Renderer2) { }
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRefer.nativeElement, 'background-color', 'blue')
-  this.renderer.setStyle(this.elRefer.nativeElement, 'color', 'white')
+  //   this.renderer.setStyle(this.elRefer.nativeElement, 'background-color', 'blue')
+  // this.renderer.setStyle(this.elRefer.nativeElement, 'color', 'white')
   }
 
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    this.renderer.setStyle(this.elRefer.nativeElement, 'background-color', 'blue');
+    this.renderer.setStyle(this.elRefer.nativeElement, 'color', 'white')
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRefer.nativeElement, 'background-color', 'transparent')
+    this.renderer.setStyle(this.elRefer.nativeElement, 'color', 'black')
+  }
 }
